cb0e371a152691c92a2829ce9d044d2336745a64
Sahar Ellouze <sahar.ellouze10@gmail.com>
2023-11-22 15:17:16 +0100

    yatzy kata : refactor largeStraight method

--- src/main/java/com/arolla/yatzy/DiceRoller.java
+++ src/main/java/com/arolla/yatzy/DiceRoller.java
@@ -10,6 +10,7 @@ public class DiceRoller {
 
     private final List<Integer> dices;
     private static final List<Integer> SMALL_STRAIGHT_LIST = List.of(1, 2, 3, 4, 5);
+    private static final List<Integer> LARGEST_STRAIGHT_LIST = List.of(2, 3, 4, 5, 6);
 
     public DiceRoller(int d1, int d2, int d3, int d4, int d5) {
         this.dices = List.of(d1, d2, d3, d4, d5);
@@ -59,5 +60,12 @@ public class DiceRoller {
                 .sorted()
                 .collect(toList())
                 .equals(SMALL_STRAIGHT_LIST);
+    }
+
+    public boolean isLargeStraight() {
+        return dices.stream()
+                .sorted()
+                .collect(toList())
+                .equals(LARGEST_STRAIGHT_LIST);
     }
 }
--- src/test/java/com/arolla/yatzy/YatzyTest.java
+++ src/test/java/com/arolla/yatzy/YatzyTest.java
@@ -116,9 +116,9 @@ public class YatzyTest {
 
     @Test
     public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
-        assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
-        assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
+        assertEquals(20, Yatzy.largeStraight(new DiceRoller(6, 2, 3, 4, 5)));
+        assertEquals(20, Yatzy.largeStraight(new DiceRoller(2, 3, 4, 5, 6)));
+        assertEquals(0, Yatzy.largeStraight(new DiceRoller(1, 2, 2, 4, 5)));
     }
 
     @Test
